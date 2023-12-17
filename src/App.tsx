4866f3aa83660684beb0e97a2559ad9ecce92386
milliorn <scottmilliorn@gmail.com>
2023-12-16 20:57:26 -0800

    Add translation functionality styling, comments to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// React imports
 import React, { ChangeEvent, FormEvent, useState } from "react";
 
 // Material UI imports
@@ -9,11 +10,27 @@ import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
+/**
+ * The endpoint for the chat completions API.
+ */
 const ENDPOINT = "https://api.openai.com/v1/chat/completions";
+
+/**
+ * The URL for the background image.
+ */
 const IMG_URL = "/background.avif";
+
+/**
+ * The URL for the source code repository.
+ */
 const SOURCE_CODE_URL =
   "https://github.com/milliorn/lord-of-the-rings-language-translator";
 
+/**
+ * Custom hook to generate styles using Material-UI's makeStyles function.
+ * @param {Theme} theme - The theme object provided by Material-UI.
+ * @returns {Object} - The generated styles object.
+ */
 const useStyles = makeStyles((theme) => ({
   root: {
     alignItems: "center",
@@ -79,11 +96,31 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+/**
+ * Props for the TranslationForm component.
+ */
 interface TranslationFormProps {
+  /**
+   * Callback function to handle translation.
+   * @param text - The text to be translated.
+   * @param language - The target language for translation.
+   */
   onTranslate: (text: string, language: string) => void;
+
+  /**
+   * Callback function to handle language change.
+   * @param language - The selected language.
+   */
   onLanguageChange: (language: string) => void;
 }
 
+/**
+ * TranslationForm component.
+ *
+ * @component
+ * @param {TranslationFormProps} props - The props for the TranslationForm component.
+ * @returns {React.ReactElement} The rendered TranslationForm component.
+ */
 const TranslationForm: React.FC<TranslationFormProps> = ({
   onTranslate,
   onLanguageChange,
@@ -91,18 +128,33 @@ const TranslationForm: React.FC<TranslationFormProps> = ({
   const [inputText, setInputText] = useState("");
   const [selectedLanguage, setSelectedLanguage] = useState("sindarin");
 
+  /**
+   * The CSS classes for the component.
+   */
   const classes = useStyles();
 
+  /**
+   * Handles the change event of the input element.
+   * @param e The change event object.
+   */
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
   };
 
+  /**
+   * Handles the change event of the select element.
+   * @param e The change event object.
+   */
   const handleLanguageChange = (e: ChangeEvent<{ value: unknown }>) => {
     const newLanguage = e.target.value as string;
     onLanguageChange(newLanguage);
     setSelectedLanguage(newLanguage);
   };
 
+  /**
+   * Handles the submit event of the form element.
+   * @param e The submit event object.
+   */
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     onTranslate(inputText, selectedLanguage);
@@ -157,9 +209,21 @@ const TranslationForm: React.FC<TranslationFormProps> = ({
   );
 };
 
+/**
+ * The App component.
+ *
+ * @component
+ * @returns {React.ReactElement} The rendered App component.
+ */
 const App = () => {
+  /**
+   * The CSS classes for the component.
+   */
   const classes = useStyles();
 
+  /**
+   * The translations state.
+   */
   const [translations, setTranslations] = useState<{ [key: string]: string }>({
     sindarin: "",
     quenya: "",
@@ -172,9 +236,19 @@ const App = () => {
     westron: "",
   });
 
+  /**
+   * The selected display language state.
+   */
   const [selectedDisplayLanguage, setSelectedDisplayLanguage] =
     useState("Sindarin");
 
+  /**
+   * Translates the given text to the specified language.
+   *
+   * @param text The text to be translated.
+   * @param language The target language for translation.
+   * @returns A Promise that resolves to the translated text.
+   */
   const handleTranslate = async (text: string, language: string) => {
     try {
       const response = await fetch(ENDPOINT, {
@@ -196,12 +270,37 @@ const App = () => {
         }),
       });
 
+      /**
+       * Checks if the response is not ok and throws an error with the status text.
+       * @param {Response} response - The response object from the translation request.
+       * @throws {Error} - Throws an error with the status text if the response is not ok.
+       */
       if (!response.ok) {
         throw new Error(`Translation request failed: ${response.statusText}`);
       }
 
+      /**
+       * Parses the response body as JSON and assigns it to the 'data' variable.
+       * @returns {Promise<any>} A promise that resolves to the parsed JSON data.
+       */
       const data = await response.json();
 
+      /**
+       * Checks if the response contains an error and throws an error with the error message.
+       * @param {any} data - The parsed JSON data from the translation request.
+       * @throws {Error} - Throws an error with the error message if the response contains an error.
+       * @returns {Promise<any>} A promise that resolves to the parsed JSON data.
+       */
+      if (data.error) {
+        throw new Error(`Translation request failed: ${data.error.message}`);
+      }
+
+      /**
+       * Checks if the response contains any choices and throws an error if it doesn't.
+       * @param {any} data - The parsed JSON data from the translation request.
+       * @throws {Error} - Throws an error if the response contains no choices.
+       * @returns {Promise<any>} A promise that resolves to the parsed JSON data.
+       */
       setTranslations((prevTranslations) => ({
         ...prevTranslations,
         [language]:
@@ -209,7 +308,10 @@ const App = () => {
           `No ${language} translation available`,
       }));
 
-      // Update the selected display language
+      /**
+       * Sets the selected display language.
+       * @param {string} language - The language to set as the selected display language.
+       */
       setSelectedDisplayLanguage(
         language.charAt(0).toUpperCase() + language.slice(1)
       );
@@ -218,6 +320,10 @@ const App = () => {
     }
   };
 
+  /**
+   * Handles the change event of the select element.
+   * @param {string} language - The selected language.
+   */
   const handleLanguageChange = (language: string) => {
     setSelectedDisplayLanguage(
       language.charAt(0).toUpperCase() + language.slice(1)
