3710848ad6ff8784f088be0906d25ca94e14ca0c
milliorn <scottmilliorn@gmail.com>
2023-12-20 19:47:39 -0800

    Fix label visibility bug and improve placeholder text opacity

--- src/App.tsx
+++ src/App.tsx
@@ -129,13 +129,13 @@ const TranslationForm: React.FC<TranslationFormProps> = ({
   const [inputText, setInputText] = useState("");
   const [selectedLanguage, setSelectedLanguage] = useState("sindarin");
   const [labelVisible, setLabelVisible] = useState(true); // State to control label visibility
-  
+
   /**
    * The CSS classes for the component.
    */
   const classes = useStyles();
 
-/**
+  /**
    * Handles the change event of the input element.
    * @param e The change event object.
    */
@@ -144,7 +144,7 @@ const TranslationForm: React.FC<TranslationFormProps> = ({
     if (labelVisible) setLabelVisible(false); // Hide label when typing starts
   };
 
-/**
+  /**
    * Handles the change event of the select element.
    * @param e The change event object.
    */
@@ -154,7 +154,7 @@ const TranslationForm: React.FC<TranslationFormProps> = ({
     setSelectedLanguage(newLanguage);
   };
 
-/**
+  /**
    * Handles the submit event of the form element.
    * @param e The submit event object.
    */
@@ -176,11 +176,11 @@ const TranslationForm: React.FC<TranslationFormProps> = ({
             color: "white",
             display: labelVisible ? "block" : "none", // Control visibility based on state
           }}
-        >
-          Click and type here
-        </InputLabel>
+        ></InputLabel>
+
         <TextField
           id="text-input"
+          placeholder="Click and type here"
           className={classes.input}
           onChange={handleInputChange}
           type="text"
@@ -198,6 +198,7 @@ const TranslationForm: React.FC<TranslationFormProps> = ({
         <Typography variant="h3" className={classes.typography}>
           Choose language
         </Typography>
+
         <Select
           className={classes.select}
           value={selectedLanguage}
@@ -234,12 +235,12 @@ const TranslationForm: React.FC<TranslationFormProps> = ({
  * @returns {React.ReactElement} The rendered App component.
  */
 const App = () => {
-/**
+  /**
    * The CSS classes for the component.
    */
   const classes = useStyles();
 
-/**
+  /**
    * The translations state.
    */
   const [translations, setTranslations] = useState<{ [key: string]: string }>({
@@ -254,13 +255,13 @@ const App = () => {
     westron: "",
   });
 
-/**
+  /**
    * The selected display language state.
    */
   const [selectedDisplayLanguage, setSelectedDisplayLanguage] =
     useState("Sindarin");
 
-/**
+  /**
    * Translates the given text to the specified language.
    *
    * @param text The text to be translated.
@@ -288,7 +289,7 @@ const App = () => {
         }),
       });
 
-/**
+      /**
        * Checks if the response is not ok and throws an error with the status text.
        * @param {Response} response - The response object from the translation request.
        * @throws {Error} - Throws an error with the status text if the response is not ok.
@@ -297,13 +298,13 @@ const App = () => {
         throw new Error(`Translation request failed: ${response.statusText}`);
       }
 
-/**
+      /**
        * Parses the response body as JSON and assigns it to the 'data' variable.
        * @returns {Promise<any>} A promise that resolves to the parsed JSON data.
        */
       const data = await response.json();
 
-/**
+      /**
        * Checks if the response contains an error and throws an error with the error message.
        * @param {any} data - The parsed JSON data from the translation request.
        * @throws {Error} - Throws an error with the error message if the response contains an error.
@@ -313,7 +314,7 @@ const App = () => {
         throw new Error(`Translation request failed: ${data.error.message}`);
       }
 
-/**
+      /**
        * Checks if the response contains any choices and throws an error if it doesn't.
        * @param {any} data - The parsed JSON data from the translation request.
        * @throws {Error} - Throws an error if the response contains no choices.
@@ -326,7 +327,7 @@ const App = () => {
           `No ${language} translation available`,
       }));
 
-/**
+      /**
        * Sets the selected display language.
        * @param {string} language - The language to set as the selected display language.
        */
@@ -338,7 +339,7 @@ const App = () => {
     }
   };
 
-/**
+  /**
    * Handles the change event of the select element.
    * @param {string} language - The selected language.
    */
